5aaf285ec898653866fc170c054e969c07d89260
Devansh Dhingra <mrdevdhingra@gmail.com>
2022-05-09 12:15:34 +0100

    js added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-let price = k;
+function testVariable() {
+    var strText = document.getElementById("textone").value;          
+    var strText1 = document.getElementById("textTWO").value;
+    var strText2 = document.getElementById("textTWO").value;
+    document.getElementById('item-name').textContent = strText;
+    document.getElementById('was-price').textContent = strText1;
+    document.getElementById('now-price').textContent = strText2; 
+    var element = document.getElementById("form");
+    element.remove();
+}
+
+function testVariable2() {
+    var strText = document.getElementById("textonetwo").value;          
+    var strText1 = document.getElementById("textTWOtwo").value;
+    var strText2 = document.getElementById("textTWOtwo").value;
+    document.getElementById('item-name2').textContent = strText;
+    document.getElementById('was-price2').textContent = strText1;
+    document.getElementById('now-price2').textContent = strText2; 
+    var element = document.getElementById("form2");
+    element.remove();
+}
+
+function display() {
+    var element = document.getElementById("printbutton");
+    element.remove();
+    window.print();
+ }
